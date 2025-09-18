08ccb5e2d69fb4ff4a736f444ec605fd1011901f
GreenMonkey85 <ccbennett@tampabay.rr.com>
2025-09-18 11:04:04 -0400

    transfered files from senior-project-roborally to robo-rally and reconnected all the file paths

--- robo-rally/Globals/Game.gd
+++ robo-rally/Globals/Game.gd
@@ -4,10 +4,10 @@ var damage_cards = []
 var damage_discards = []
 var upgrade_cards = []
 var upgrade_discards = []
-var character_to_color = {"Twonky": "orange",
- 						  "Hammer Bot": "purple",
-						  "Spin Bot": "blue",
- 						  "Robby": "white"}
+var character_to_color = {"Twonky": "Orange",
+ 						  "Hammer Bot": "Purple",
+						  "Spin Bot": "Blue",
+ 						  "Robby": "White"}
 
 func start_game(player_list, chosen_board):
 	player_order
