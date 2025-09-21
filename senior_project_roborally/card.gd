c23309ba4533f072802a3ba24410b12a532acb6d
bkriner <krinerbk@hotmail.com>
2025-09-21 19:18:46 -0400

    card hand in progress

---
+++ senior_project_roborally/card.gd
@@ -0,0 +1,29 @@
+extends Container
+
+@onready var card = preload("res://cardHolder.tscn")
+var startPosition
+var cardHighlighted = false
+
+func _on_mouse_entered() -> void:
+	$Anim.play("Select")
+	cardHighlighted = true
+
+func _on_mouse_exited() -> void:
+	$Anim.play("Deselect")
+	cardHighlighted = false
+
+func _on_gui_input(event):
+	if (event is InputEventMouseButton) and (event.button_index == 1):
+		if event.button_mask == 1:
+			# pressed down
+			if cardHighlighted:
+				var cardTemp = card.instantiate()
+				get_tree().get_root().get_node("Board/CardHolder").add_child(cardTemp)
+				Game.cardSelected = true
+				if cardHighlighted:
+					self.get_child(0).hide()
+		elif event.button_mask == 0:
+			# press up
+			if !Game.mouseOnPlacement:
+				cardHighlighted = false
+				self.get_child(0).show()
