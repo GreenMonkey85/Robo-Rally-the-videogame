130955e98c2f8e406726acfde547455ddd8eac0c
GreenMonkey85 <ccbennett@tampabay.rr.com>
2025-09-21 15:48:36 -0400

    added scene transition from character to board selection

--- robo-rally/Globals/Game.gd
+++ robo-rally/Globals/Game.gd
@@ -1,12 +1,27 @@
 extends Node
+
 var player_order = []
 var damage_cards = []
 var damage_discards = []
 var upgrade_cards = []
 var upgrade_discards = []
+var registers = []
+var timer = Timer.new()
 
-
-
+func action_round():
+	pass
+	
+func decision_round():
+	for player in player_order:
+		player.decision()
+		
 
 func start_game(player_list, chosen_board):
-	player_order
+	player_order = player_list.shuffle()
+	
+	decision_round()
+	
+	
+	
+	
+	
--- robo-rally/Scenes/Characters/robot.gd
+++ robo-rally/Scenes/Characters/robot.gd
@@ -7,26 +7,37 @@ var energy = 3
 var checkpoints = 0
 
 
+var deck = ["res://Resources/Cards/Movement_Cards/move1.tres", "res://Resources/Cards/Movement_Cards/move1.tres",
+			"res://Resources/Cards/Movement_Cards/move1.tres", "res://Resources/Cards/Movement_Cards/move1.tres",
+			"res://Resources/Cards/Movement_Cards/rotate_left.tres", "res://Resources/Cards/Movement_Cards/rotate_left.tres",
+			"res://Resources/Cards/Movement_Cards/rotate_left.tres", "res://Resources/Cards/Movement_Cards/rotate_left.tres",
+			"res://Resources/Cards/Movement_Cards/rotate_right.tres", "res://Resources/Cards/Movement_Cards/rotate_right.tres",
+			"res://Resources/Cards/Movement_Cards/rotate_right.tres", "res://Resources/Cards/Movement_Cards/rotate_right.tres",
+			"res://Resources/Cards/Movement_Cards/move2.tres", "res://Resources/Cards/Movement_Cards/move2.tres",
+			"res://Resources/Cards/Movement_Cards/move2.tres", "res://Resources/Cards/Movement_Cards/again.tres",
+			"res://Resources/Cards/Movement_Cards/move_back.tres", "res://Resources/Cards/Movement_Cards/uturn.tres",
+			"res://Resources/Cards/Movement_Cards/move3.tres", "res://Resources/Cards/Movement_Cards/power_up.tres"]
 
-var deck = preload("res://Resources/Cards/Movement_Cards/movement_deck.tres")
 var discard = []
 var cards_in_hand = []
 var current_permanent_upgrades = []
 var current_temporary_upgrades = []
 var upgrades_in_hand = []
-var register = [5]
+var register = []
+
+func decision():
+	pass
 
 func _ready() -> void:
 	
 	# Create deck of cards for specific characterand set correct sprite for each
-	for card in deck.cards:
+	for card in deck:
 		card.character = character
 		card.sprite = load("res://Graphics/CardSprites/%s_cards/%s_%s_card.png"
 							 % [character.to_lower(), character.to_lower(), card.name])
-	
+
 	#Shuffle
 	deck.shuffle()
 
-
-	for card in deck:
+	for card in deck.cards:
 		print(card.character, card.name)
