987d291835b45e3d3e16433a74033bb0d45c4b91
bkriner <krinerbk@hotmail.com>
2025-09-25 11:23:29 -0400

    place card in register

--- senior_project_roborally/card_container.gd
+++ senior_project_roborally/card_container.gd
@@ -1,9 +1,18 @@
 extends HBoxContainer
 
+@onready var card = preload("res://cardOnBoard.tscn")
 
 func _on_mouse_entered() -> void:
-	pass # Replace with function body.
+	Game.mouseOnPlacement = true
 
 
 func _on_mouse_exited() -> void:
-	pass # Replace with function body.
+	Game.mouseOnPlacement = false
+	
+
+func placeCard():
+	var cardTemp = card.card_instantiate()
+	var projectionResolution = ProjectSettings.get_setting("display/window/size/viewport_width")
+	var projectResolutionHeight = ProjectSettings.get_setting("display/window/size/viewport_height")
+	cardTemp.global_position = Vector2(projectionResolution/2, projectResolutionHeight/2) - self.position
+	add_child(cardTemp)
