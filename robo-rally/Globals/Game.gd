de16868a20ad1dfcfab05b609eb3c2d9ace2a1a7
GreenMonkey85 <ccbennett@tampabay.rr.com>
2025-10-02 11:26:45 -0400

    finished a lot of the game loop and added robot movement templates

--- robo-rally/Globals/Game.gd
+++ robo-rally/Globals/Game.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 var cardSelected
 var mouseOnPlacement = false
 
@@ -9,23 +8,54 @@ var damage_cards = []
 var damage_discards = []
 var upgrade_cards = []
 var upgrade_discards = []
+
+var players_decided = []
 var registers = []
+
 var timer = Timer.new()
 
 func action_round():
-	pass
-	
+	# loop through each register slot
+	for i in range(len(registers[0])):
+		# loop through each player with respect to order
+		for j in range(len(player_order)):
+			# current player moves
+			player_order[j].call(registers[j][i].Action)
+		# double conveyers
+		# single conveyer
+		# push panels
+		# rotate gears
+		# board lasers
+		# robot lasers
+		# battery
+		# check flags
+	decision_round()
+
 func decision_round():
 	for player in player_order:
 		player.decision()
-		
+	timer.start(60.0)
+
+func _on_all_decided(player, register):
+	
+	if player not in players_decided:
+		players_decided.append(player)
+		for i in range(len(player_order)):
+			if player_order[i] == player:
+				registers[i] = register
+				break
+
+	if len(players_decided) >= len(player_order):
+		players_decided.clear()
+		action_round()
 
 func start_game(player_list, chosen_board):
 	player_order = player_list.shuffle()
-	
+	for i in range(len(player_order)):
+		registers.append(null)
+
+
+
+
 	decision_round()
-	
-	
-	
-	
 	
--- robo-rally/Scenes/Characters/robot.gd
+++ robo-rally/Scenes/Characters/robot.gd
@@ -3,9 +3,10 @@ extends CharacterBody2D
 var player: String
 var character: String
 
+signal player_decision_end
+
 var energy = 3
 var checkpoints = 0
-
 
 var deck = ["res://Resources/Cards/Movement_Cards/move1.tres", "res://Resources/Cards/Movement_Cards/move1.tres",
 			"res://Resources/Cards/Movement_Cards/move1.tres", "res://Resources/Cards/Movement_Cards/move1.tres",
@@ -23,12 +24,51 @@ var cards_in_hand = []
 var current_permanent_upgrades = []
 var current_temporary_upgrades = []
 var upgrades_in_hand = []
-var register = []
+var register = [null,null,null,null,null]
 
-func decision():
+func decision_start():
+	while cards_in_hand.size() <= 9:
+		if deck <= 0:
+			deck = discard
+			discard.clear()
+			deck.shuffle()
+		cards_in_hand.append(deck.pop_front())
+	cards_in_hand.sort()
+	
+func decision_end():
+	for i in cards_in_hand:
+		if i.Type == "Movement":
+			discard.append(cards_in_hand.pop_at(i))
+			
+	player_decision_end.emit(player, register)
+	
+  
+func Move(num_actions):
 	pass
+	
+func MoveBack(num_actions):
+	pass
+	
+func Again(num_actions):
+	pass
+	
+func RotateRight(num_actions):
+	pass
+	
+func RotateLeft(num_actions):
+	pass
+	
+func Uturn(num_actions):
+	pass
+	
+func PowerUp(num_actions):
+	pass
+	
+
 
 func _ready() -> void:
+	#
+	player_decision_end.connect(Callable(self, "_on_all_decided"))
 	
 	# Create deck of cards for specific characterand set correct sprite for each
 	for card in deck:
@@ -36,8 +76,5 @@ func _ready() -> void:
 		card.sprite = load("res://Graphics/CardSprites/%s_cards/%s_%s_card.png"
 							 % [character.to_lower(), character.to_lower(), card.name])
 
-	#Shuffle
+	# Shuffle cards
 	deck.shuffle()
-
-	for card in deck.cards:
-		print(card.character, card.name)
